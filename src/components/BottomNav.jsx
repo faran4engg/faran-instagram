34fa12582449be4eb051f5969bef4923c538b3a8
faran4engg <faran4engg@gmail.com>
2020-08-02 00:41:17 +0200

    dialog changes

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -7,6 +7,15 @@ import HomeIcon from '@material-ui/icons/Home';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import SearchIcon from '@material-ui/icons/Search';
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import Slide from '@material-ui/core/Slide';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import './BottomNav.css';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles({
   root: {
@@ -18,37 +27,129 @@ const useStyles = makeStyles({
   },
 });
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />;
+});
+
 export default function BottomNav() {
   const classes = useStyles();
   const [value, setValue] = React.useState('home');
+  const [open, setOpen] = React.useState(false);
+
+  const [imagePreviewUrl, setImagePreviewUrl] = React.useState('');
+  const [file, setFile] = React.useState('');
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  let $imagePreview = null;
+  if (imagePreviewUrl) {
+    $imagePreview = <img src={imagePreviewUrl} alt='post img' />;
+  } else {
+    $imagePreview = (
+      <div className='previewText'>Please select an Image for Preview</div>
+    );
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', file);
+  };
+
+  const handleImageChange = (e) => {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      setFile(file);
+      setImagePreviewUrl(reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <BottomNavigation
-      value={value}
-      onChange={handleChange}
-      className={classes.root}
-    >
-      <BottomNavigationAction label='Home' value='home' icon={<HomeIcon />} />
-      <BottomNavigationAction
-        label='Search'
-        value='search'
-        icon={<SearchIcon />}
-      />
-      <BottomNavigationAction label='Add' value='add' icon={<AddBoxIcon />} />
-      <BottomNavigationAction
-        label='Favorites'
-        value='favorites'
-        icon={<FavoriteIcon />}
-      />
-      <BottomNavigationAction
-        label='Profile'
-        value='Peprofile'
-        icon={<PersonIcon />}
-      />
-    </BottomNavigation>
+    <>
+      <BottomNavigation
+        value={value}
+        onChange={handleChange}
+        className={classes.root}
+      >
+        <BottomNavigationAction label='Home' value='home' icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label='Search'
+          value='search'
+          icon={<SearchIcon />}
+        />
+        <BottomNavigationAction
+          label='Add'
+          value='add'
+          icon={<AddBoxIcon />}
+          onClick={handleClickOpen}
+        />
+        <BottomNavigationAction
+          label='Favorites'
+          value='favorites'
+          icon={<FavoriteIcon />}
+        />
+        <BottomNavigationAction
+          label='Profile'
+          value='Peprofile'
+          icon={<PersonIcon />}
+        />
+      </BottomNavigation>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              edge='start'
+              color='inherit'
+              onClick={handleClose}
+              aria-label='close'
+            >
+              <CloseIcon />
+            </IconButton>
+
+            <Button autoFocus color='inherit' onClick={handleClose}>
+              Add Post
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <div className='file-upload-wrapper'>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <input
+                className='fileInput'
+                type='file'
+                onChange={(e) => handleImageChange(e)}
+              />
+              <button
+                className='submitButton'
+                type='submit'
+                onClick={(e) => handleSubmit(e)}
+              >
+                Upload Image
+              </button>
+            </form>
+            <div className='imgPreview'>{$imagePreview}</div>
+          </div>
+        </Container>
+      </Dialog>
+    </>
   );
 }
