e42c146a1115ad41a7382795c58a065fec528c5f
faran4engg <faran4engg@gmail.com>
2020-08-02 11:21:44 +0200

    embedding insta

--- src/pages/Home/Areas/HighlightArea/HighlightArea.jsx
+++ src/pages/Home/Areas/HighlightArea/HighlightArea.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
+import InstagramEmbed from 'react-instagram-embed';
 
 const HighlightArea = () => {
   return (
-    <div style={{ backgroundColor: 'green' }}>
-      2222222222222222222222222222222222 Lorem ipsum dolor sit amet consectetur,
-      adipisicing elit. Sequi consectetur ad maxime nisi, asperiores nobis.
-      Eligendi amet porro odit saepe minima assumenda quasi minus ratione cumque
-      dolor molestiae suscipit explicabo recusandae, odio nobis, illo numquam.
-      Animi suscipit illum at officia dolore fugiat inventore rerum expedita
-      illo accusamus voluptates id dicta reiciendis, facilis asperiores laborum
-      quae doloremque itaque architecto modi. Facilis similique tenetur magnam
-      nulla fugit sequi ad, eligendi molestiae rem suscipit at quaerat,
-      distinctio cumque saepe ut explicabo adipisci sunt doloremque sit ullam?
-      Similique illo omnis odit, repellendus quasi nemo modi corporis quas
-      officiis ipsam eum consectetur ullam non nulla.
-    </div>
+    <InstagramEmbed
+      url='https://www.instagram.com/p/B7ouFQHA-C0/'
+      hideCaption={false}
+      containerTagName='div'
+      protocol=''
+      injectScript
+      onLoading={() => {}}
+      onSuccess={() => {}}
+      onAfterRender={() => {}}
+      onFailure={() => {}}
+    />
   );
 };
 
